68b9d785354773aa1eed0b0f3a545d469b5e938c
gonchigars <gonchigars@gmail.com>
2024-06-04 19:25:34 +0530

    Employee

---
+++ src/main/java/com/example/Book.java
@@ -0,0 +1,22 @@
+/*
+ * Requirements
+Class Definition:
+
+Define a class named Book.
+Instance Variables:
+
+The Book class should have private instance variables for the title (String), author (String), and number of copies (int).
+Constructor:
+
+The class should have a constructor that initializes the title, author, and number of copies of the book.
+Getters and Setters:
+
+Implement getter and setter methods for each instance variable.
+The getter for the number of copies should return the current number of copies available.
+The setter for the number of copies should allow updating the number of copies.
+Additional Methods:
+
+Implement a method displayBookDetails that prints the details of the book (title, author, and number of copies).
+Implement a static method totalBooks that returns the total number of books created (use a static variable to keep track of the count).
+ * 
+ */
